79e73f59f90b65704d9d4e2ea22cd83f133ca7ff
JoschD <joschua.dilly@gmail.com>
2019-08-01 17:24:36 +0200

    #5 Added version and bumpversion.cfg

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,4 @@
+__version__ = "1.0.0"
 # ignore numpy warnings, see:
 # https://stackoverflow.com/questions/40845304/runtimewarning-numpy-dtype-size-changed-may-indicate-binary-incompatibility
 import warnings
