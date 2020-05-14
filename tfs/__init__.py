41a485d834004d521ce7efcaa95ac94af948a5cc
JoschD <joschua.dilly@gmail.com>
2020-05-14 22:04:58 +0200

    Release1.0.4 (#39)
    * Bump version: 1.0.3 → 1.0.4
    
    * Updated CHANGELOG
    
    * Updated travis.yml to run doc on master commits

--- tfs/__init__.py
+++ tfs/__init__.py
@@ -1,6 +1,6 @@
 """Exposes TfsDataFrame, read_tfs and write_tfs directly in tfs namespace."""
 from tfs.handler import read_tfs, write_tfs, TfsDataFrame
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 # aliases
 read = read_tfs
 write = write_tfs
