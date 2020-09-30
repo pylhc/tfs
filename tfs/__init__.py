3a36f4cf0594c4aab758abe24e690eefc0bd0e53
Felix Soubelet <felix.soubelet@protonmail.com>
2020-09-30 14:04:48 +0200

    enhancement(#52): last commit for fix release
    - changelog
    - version number

--- tfs/__init__.py
+++ tfs/__init__.py
@@ -6,7 +6,7 @@ from tfs.handler import TfsDataFrame, read_tfs, write_tfs
 __title__ = "tfs-pandas"
 __description__ = "Read and write tfs files."
 __url__ = "https://github.com/pylhc/tfs"
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __author__ = "pylhc"
 __author_email__ = "pylhc@github.com"
 __license__ = "MIT"
