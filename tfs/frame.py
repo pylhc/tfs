b6f8f59386af94d910cf800b5f15371a7d8653f3
Felix Soubelet <felix.soubelet@protonmail.com>
2021-08-19 19:25:38 +0200

    structure for overridden functionality (append, join, merge and concat). Still need to determine how we want to handle the headers merging.

--- tfs/frame.py
+++ tfs/frame.py
@@ -7,12 +7,14 @@ as a utility function to validate the correctness of a ``TfsDataFrame``.
 """
 import logging
 from contextlib import suppress
-from typing import Sequence, Union
+from functools import partial, reduce
+from typing import Optional, Sequence, Union
 
 import numpy as np
 import pandas as pd
 
 from tfs.errors import TfsFormatError
+from tfs.tools import merge_headers
 
 LOGGER = logging.getLogger(__name__)
 
@@ -74,6 +76,143 @@ class TfsDataFrame(pd.DataFrame):
             s += "\n"
         return f"{s}{super().__repr__()}"
 
+    def append(
+        self, other: "TfsDataFrame", how_headers: str, new_headers: dict = None, **kwargs
+    ) -> "TfsDataFrame":
+        """
+        Append rows of the other ``TfsDataFrame`` to the end of caller, returning a new object. Data
+        manipulation is done by the ``pandas.Dataframe`` method of the same name. Resulting new_headers are
+        either merged according to the provided **how_headers** method or as given via **new_headers**.
+
+        Args:
+            other (TfsDataFrame): The ``TfsDataFrame`` to append to the caller.
+            how_headers (str): TODO.
+            new_headers (dict): If provided, will be used as new_headers for the merged ``TfsDataFrame``.
+                Otherwise these are determined by merging the headers from the caller and the other
+                ``TfsDataFrame`` according to the method defined by the **how_headers** argument.
+
+        Keyword Args:
+            Any keyword argument is given to ``pandas.DataFrame.append()``. The default values for all these
+            parameters are left as set in the ``pandas`` codebase. To see these, refer to the pandas
+            [DataFrame.append documentation](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.append.html).
+
+        Returns:
+            A new ``TfsDataFrame`` with the appended data and merged new_headers.
+        """
+        LOGGER.debug("Appending data through 'pandas'")
+        dframe = pd.DataFrame(self).append(other, **kwargs)
+
+        LOGGER.debug("Determining headers")
+        new_headers = (
+            new_headers
+            if new_headers is not None
+            else merge_headers(self.headers, other.headers, how=how_headers)
+        )
+        return TfsDataFrame(data=dframe, headers=new_headers)
+
+    def join(
+        self, other: "TfsDataFrame", how_headers: str, new_headers: dict = None, **kwargs
+    ) -> "TfsDataFrame":
+        """
+        Join columns of another ``TfsDataFrame``. Data manipulation is done by the ``pandas.Dataframe``
+        method of the same name. Resulting new_headers are either merged according to the provided
+        **how_headers** method or as given via **new_headers**.
+
+        Args:
+            other (TfsDataFrame): The ``TfsDataFrame`` to join into the caller.
+            how_headers (str): TODO.
+            new_headers (dict): If provided, will be used as new_headers for the merged``TfsDataFrame``.
+                Otherwise these are determined by merging the headers from the caller and the other
+                ``TfsDataFrame`` according to the method defined by the **how_headers** argument.
+
+        Keyword Args:
+            Any keyword argument is given to ``pandas.DataFrame.join()``. The default values for all these
+            parameters are left as set in the ``pandas`` codebase. To see these, refer to the pandas
+            [DataFrame.join documentation](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.join.html).
+
+        Returns:
+            A new ``TfsDataFrame`` with the joined columns and merged new_headers.
+        """
+        LOGGER.debug("Joining data through 'pandas'")
+        dframe = pd.DataFrame(self).join(other, **kwargs)
+
+        LOGGER.debug("Determining headers")
+        new_headers = (
+            new_headers
+            if new_headers is not None
+            else merge_headers(self.headers, other.headers, how=how_headers)
+        )
+        return TfsDataFrame(data=dframe, headers=new_headers)
+
+    def merge(
+        self, right: "TfsDataFrame", how_headers: str, new_headers: dict = None, **kwargs
+    ) -> "TfsDataFrame":
+        """
+        Merge ``TfsDataFrame`` objects with a database-style join. Data manipulation is done by the
+        ``pandas.Dataframe`` method of the same name. Resulting new_headers are either merged according to the
+        provided **how_headers** method or as given via **new_headers**.
+
+        Args:
+            right (TfsDataFrame): The ``TfsDataFrame`` to merge with the caller.
+            how_headers (str): TODO.
+            new_headers (dict): If provided, will be used as headers for the merged ``TfsDataFrame``.
+                Otherwise these are determined by merging the headers from the caller and the other
+                ``TfsDataFrame`` according to the method defined by the **how_headers** argument.
+
+        Keyword Args:
+            Any keyword argument is given to ``pandas.DataFrame.merge()``. The default values for all these
+            parameters are left as set in the ``pandas`` codebase. To see these, refer to the pandas
+            [DataFrame.merge documentation](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.merge.html).
+
+        Returns:
+            A new ``TfsDataFrame`` with the merged data and merged new_headers.
+        """
+        LOGGER.debug("Merging data through 'pandas'")
+        dframe = pd.DataFrame(self).merge(right, **kwargs)
+
+        LOGGER.debug("Determining headers")
+        new_headers = (
+            new_headers
+            if new_headers is not None
+            else merge_headers(self.headers, right.headers, how=how_headers)
+        )
+        return TfsDataFrame(data=dframe, headers=new_headers)
+
+
+def concat(
+    objs: Sequence[TfsDataFrame],
+    how_headers: str,
+    new_headers: dict = None,
+    **kwargs,
+) -> TfsDataFrame:
+    """
+    Concatenate ``TfsDataFrame`` objects along a particular axis with optional set logic along the other
+    axes. Data manipulation is done by the ``pandas.concat`` function. Resulting new_headers are either
+    merged according to the provided **how_headers** method or as given via **new_headers**.
+
+    Args:
+        objs (Sequence[TfsDataFrame]): the ``TfsDataFrame`` objects to be concatenated.
+        how_headers (str): TODO.
+        new_headers (dict): If provided, will be used as headers for the merged ``TfsDataFrame``.
+            Otherwise these are determined by successively merging the headers from all concatenated
+            ``TfsDataFrames`` according to the method defined by the **how_headers** argument.
+
+    Keyword Args:
+        Any keyword argument is given to ``pandas.concat()``. The default values for all these parameters
+        are left as set in the ``pandas`` codebase. To see these, refer to the [pandas.concat
+        documentation](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.concat.html).
+
+    Returns:
+        A new ``TfsDataFrame`` with the merged data and merged new_headers.
+    """
+    LOGGER.debug("Concatenating data through 'pandas'")
+    dframe = pd.concat(objs, **kwargs)
+
+    LOGGER.debug("Determining headers")
+    merger = partial(merge_headers, how=how_headers)  # so we can reduce on all objs, and use how_headers
+    new_headers = new_headers if new_headers is not None else reduce(merger, objs)
+    return TfsDataFrame(data=dframe, headers=new_headers)
+
 
 def validate(
     data_frame: Union[TfsDataFrame, pd.DataFrame],
@@ -81,7 +220,7 @@ def validate(
     non_unique_behavior: str = "warn",
 ) -> None:
     """
-    Check if a data frame contains finite values only, strings as column names and no empty headers
+    Check if a data frame contains finite values only, strings as column names and no empty new_headers
     or column names.
 
     Args:
@@ -129,7 +268,7 @@ def validate(
         LOGGER.error(f"Space(s) found in TFS columns, dataframe {info_str} is invalid")
         raise TfsFormatError("TFS-Columns can not contain spaces.")
 
-    if hasattr(data_frame, "headers") and any(" " in h for h in data_frame.headers.keys()):
+    if hasattr(data_frame, "new_headers") and any(" " in h for h in data_frame.headers.keys()):
         LOGGER.error(f"Space(s) found in TFS header names, dataframe {info_str} is invalid")
         raise TfsFormatError("TFS-Header names can not contain spaces.")
 
