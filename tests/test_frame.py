6951b6a16299fd51716b9967bc55d783dd12642f
Felix Soubelet <felix.soubelet@protonmail.com>
2021-08-26 15:07:00 +0200

    tests for headers merging

--- tests/test_frame.py
+++ tests/test_frame.py
@@ -1,9 +1,10 @@
 import pathlib
+from collections import OrderedDict
 
 import pytest
 
 import tfs
-from tfs import TfsDataFrame
+from tfs.frame import TfsDataFrame, concat, merge_headers, validate
 
 CURRENT_DIR = pathlib.Path(__file__).parent
 
@@ -12,12 +13,41 @@ class TestFailures:
     def test_validate_raises_on_wrong_unique_behavior(self, caplog):
         df = TfsDataFrame(index=["A", "B", "A"], columns=["A", "B", "A"])
         with pytest.raises(KeyError):
-            tfs.frame.validate(df, "", non_unique_behavior="invalid")
+            validate(df, "", non_unique_behavior="invalid")
+
+    @pytest.mark.parametrize("how", ["invalid", "not_left", "not_right"])
+    def test_merge_headers_raises_on_invalid_how_key(self, caplog, how):
+        headers_left = OrderedDict()
+        headers_right = OrderedDict()
+
+        with pytest.raises(ValueError):
+            merge_headers(headers_left, headers_right, how=how)
 
 
 class TestMerges:
-    def test_qq(self):
-        pass
+    @pytest.mark.parametrize("how", ["left", "LEFT", "Left", "lEfT"])  # we're case-insensitive
+    def test_headers_merging_left(self, _tfs_file_x_pathlib, _tfs_file_y_pathlib, how):
+        headers_left = tfs.read(_tfs_file_x_pathlib).headers
+        headers_right = tfs.read(_tfs_file_y_pathlib).headers
+        result = merge_headers(headers_left, headers_right, how=how)
+
+        assert isinstance(result, OrderedDict)
+        assert len(result) >= len(headers_left) and len(result) >= len(headers_right)  # no key disappeared
+        for key in result:  # check that we prioritized headers_left's contents
+            if key in headers_left and key in headers_right:
+                assert result[key] == headers_left[key]
+
+    @pytest.mark.parametrize("how", ["right", "RIGHT", "Right", "RigHt"])  # we're case-insensitive
+    def test_headers_merging_right(self, _tfs_file_x_pathlib, _tfs_file_y_pathlib, how):
+        headers_left = tfs.read(_tfs_file_x_pathlib).headers
+        headers_right = tfs.read(_tfs_file_y_pathlib).headers
+        result = merge_headers(headers_left, headers_right, how=how)
+
+        assert isinstance(result, OrderedDict)
+        assert len(result) >= len(headers_left) and len(result) >= len(headers_right)  # no key disappeared
+        for key in result:  # check that we prioritized headers_right's contents
+            if key in headers_left and key in headers_right:
+                assert result[key] == headers_right[key]
 
 
 class TestPrinting:
@@ -30,3 +60,16 @@ class TestPrinting:
         for key, val in headers.items():
             assert key in print_out
             assert str(val) in print_out
+
+
+# ------ Fixtures ------ #
+
+
+@pytest.fixture()
+def _tfs_file_x_pathlib() -> pathlib.Path:
+    return CURRENT_DIR / "inputs" / "file_x.tfs"
+
+
+@pytest.fixture()
+def _tfs_file_y_pathlib() -> pathlib.Path:
+    return CURRENT_DIR / "inputs" / "file_x.tfs"
