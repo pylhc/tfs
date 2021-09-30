a99c369753d177227731e926434e9e4dd135e282
Felix Soubelet <felix.soubelet@protonmail.com>
2021-09-30 16:06:06 +0200

    test for append method with other being a pandas dataframe

--- tests/test_frame.py
+++ tests/test_frame.py
@@ -110,6 +110,19 @@ class TestTfsDataFrameAppending:
         assert_dict_equal(result.headers, merge_headers(dframe_x.headers, dframe_y.headers, how=how_headers))
         assert_frame_equal(result, pd.DataFrame(dframe_x).append(pd.DataFrame(dframe_y)))
 
+    @pytest.mark.parametrize("how_headers", [None, "left", "right"])
+    def test_appending_accepts_pandas_dataframe(self, _tfs_file_x_pathlib, _tfs_file_y_pathlib, how_headers):
+        dframe_x = tfs.read(_tfs_file_x_pathlib)
+        dframe_y = pd.DataFrame(tfs.read(_tfs_file_y_pathlib))  # for test, loses headers here
+        result = dframe_x.append(dframe_y, how_headers=how_headers)
+
+        assert isinstance(result, TfsDataFrame)
+        assert isinstance(result.headers, OrderedDict)
+
+        # using empty OrderedDict here as it's what dframe_y is getting when converted in the call
+        assert_dict_equal(result.headers, merge_headers(dframe_x.headers, OrderedDict(), how=how_headers))
+        assert_frame_equal(result, pd.DataFrame(dframe_x).append(dframe_y))  # dframe_y already pandas
+
 
 class TestTfsDataFrameJoining:
     @pytest.mark.parametrize("how_headers", [None, "left", "right"])
