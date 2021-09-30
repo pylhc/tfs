a058787080295fdcff244a492b409f0515facc98
Felix Soubelet <felix.soubelet@protonmail.com>
2021-09-30 16:20:57 +0200

    this is actually a fix

--- tfs/__init__.py
+++ tfs/__init__.py
@@ -9,7 +9,7 @@ from tfs.writer import write_tfs
 __title__ = "tfs-pandas"
 __description__ = "Read and write tfs files."
 __url__ = "https://github.com/pylhc/tfs"
-__version__ = "3.1.0"
+__version__ = "3.0.1"
 __author__ = "pylhc"
 __author_email__ = "pylhc@github.com"
 __license__ = "MIT"
