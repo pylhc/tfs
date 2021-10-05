5159a6d1fa936d8c04e79c4a323208b1d1f9c5cb
Felix Soubelet <19598248+fsoubelet@users.noreply.github.com>
2021-10-05 12:46:37 +0000

    documentation build fix for sphinx-rtd-theme > 1.0 (#92)

--- doc/conf.py
+++ doc/conf.py
@@ -79,7 +79,7 @@ master_doc = "index"
 
 # General information about the project.
 project = ABOUT_TFS["__title__"]
-copyright_ = "2018-2021, pyLHC/OMC-TEAM"
+copyright_ = "2018, pyLHC/OMC-TEAM"
 author = ABOUT_TFS["__author__"]
 
 # Override link in 'Edit on Github'
@@ -131,13 +131,13 @@ html_theme_options = {
 html_logo = "_static/img/omc_logo.svg"
 html_static_path = ["_static"]
 html_context = {
-    "css_files": ["_static/css/custom.css"],
     "display_github": True,
     # the following are only needed if :github_url: is not set
     "github_user": author,
     "github_repo": project,
     "github_version": "master/doc/",
 }
+html_css_files = ["css/custom.css"]
 
 smartquotes_action = "qe"  # renders only quotes and ellipses (...) but not dashes (option: D)
 
