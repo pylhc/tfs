5dfdeaedd53522bff69324efe92988a84b4ed622
JoschD <26184899+JoschD@users.noreply.github.com>
2022-05-30 15:19:23 +0200

    python requires to 3.7 as 3.6 not tested and not in classifiers

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setuptools.setup(
     url=ABOUT_TFS["__url__"],
     packages=setuptools.find_packages(include=(MODULE_NAME,)),
     include_package_data=True,
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     license=ABOUT_TFS["__license__"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
