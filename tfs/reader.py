529487c5910aaf91ac2951fe704357a90384385f
Felix Soubelet <felix.soubelet@protonmail.com>
2023-03-09 16:51:49 +0000

    helper to get metadata and custom dataclass, split tasks and make function to read only headers

--- tfs/reader.py
+++ tfs/reader.py
@@ -8,6 +8,7 @@ import logging
 import pathlib
 import shlex
 from collections import OrderedDict
+from dataclasses import dataclass
 from typing import List, Union
 
 import numpy as np
@@ -22,21 +23,23 @@ LOGGER = logging.getLogger(__name__)
 
 # A string not expected to be found in the headers
 # I generated this randomly with: "".join(random.choice(string.ascii_letters) for _ in range(100))
-_UNEXPECTED_SEP: str = "baBfHIhwOdMnuBVHDDZcysbmwRgWaBnukQPIWNHpFVqjrIcOryhvyJwIRRHfqOQLGKhtZPLJhziZKomfVhXsoqfoGkvyFKuNhhst"
+_UNEXPECTED_SEP: str = (
+    "baBfHIhwOdMnuBVHDDZcysbmwRgWaBnukQPIWNHpFVqjrIcOryhvyJwIRRHfqOQLGKhtZPLJhziZKomfVhXsoqfoGkvyFKuNhhst"
+)
+
 
 def read_tfs(
     tfs_file_path: Union[pathlib.Path, str],
     index: str = None,
     non_unique_behavior: str = "warn",
     validate: bool = True,
-    headers_only: bool = False,
 ) -> TfsDataFrame:
     """
     Parses the **TFS** table present in **tfs_file_path** and returns a ``TfsDataFrame``.
 
     .. note::
         Loading and reading compressed files is possible. Any compression format supported
-        by ``pandas`` is accepted, which includes: ``.gz``, ``.bz2``, ``.zip``, ``.xz``, 
+        by ``pandas`` is accepted, which includes: ``.gz``, ``.bz2``, ``.zip``, ``.xz``,
         ``.zst``, ``.tar``, ``.tar.gz``, ``.tar.xz`` or ``.tar.bz2``. See below for examples.
 
     .. warning::
@@ -49,12 +52,13 @@ def read_tfs(
 
     .. admonition:: **Methodology**
 
-        This function parses the first lines of the file until it gets to the `types` line.
-        While parsed, the appropriate information is gathered (headers content, column names & types,
-        number of lines parsed). After reaching the `types` line, the rest of the file is given to parse
-        to ``pandas.read_csv`` with the right options to make use of its C engine's speed. After this,
-        conversion to ``TfsDataDrame`` is made, proper types are applied to columns, the index is set and
-        the frame is validated before being returned.
+        This function first calls a helper which parses and returns all metadata
+        from the file (headers content, column names & types, number of lines
+        parsed). The rest of the file (dataframe part) is given to parse to
+        ``pandas.read_csv`` with the right options to make use of its C engine's
+        speed. After this, conversion to ``TfsDataDrame`` is made, proper types
+        are applied to columns, the index is set and the frame is eventually
+        validated before being returned.
 
     Args:
         tfs_file_path (Union[pathlib.Path, str]): Path object to the **TFS** file to read. Can be
@@ -65,9 +69,6 @@ def read_tfs(
             dataframe. Accepts `warn` and `raise` as values, case-insensitively, which dictates
             to respectively issue a warning or raise an error if non-unique elements are found.
         validate (bool): Whether to validate the dataframe after reading it. Defaults to ``False``.
-        headers_only (bool): Whether to only read the headers of the file and not the data. If set
-            to ``True``, then an empty dataframe is returned but with the headers loaded. Defaults
-            to ``False``.
 
     Returns:
         A ``TfsDataFrame`` object with the loaded data from the file.
@@ -98,14 +99,14 @@ def read_tfs(
         .. code-block:: python
 
             >>> tfs.read("filename.tfs", non_unique_behavior="raise")
-        
+
         One can choose to skip dataframe validation **at one's own risk** after reading
         from file. This is done as:
 
         .. code-block:: python
 
             >>> tfs.read("filename.tfs", validate=False)
-        
+
         It is possible to load compressed files if the compression format is supported by pandas.
         (see above). The compression format detection is handled automatically from the extension
         of the provided **tfs_file_path** suffix. For instance:
@@ -117,13 +118,126 @@ def read_tfs(
             >>> tfs.read("filename.tfs.zip")
     """
     tfs_file_path = pathlib.Path(tfs_file_path)
+    LOGGER.debug(f"Reading path: {tfs_file_path.absolute()}")
+
+    # First step: get the metadata from the file ()
+    metadata: _TfsMetaData = _read_metadata(tfs_file_path)
+
+    if metadata.column_names is None:
+        raise TfsFormatError(f"No column names in file {tfs_file_path.absolute()}. File not read.")
+    if metadata.column_types is None:
+        raise TfsFormatError(f"No column types in file {tfs_file_path.absolute()}. File not read.")
+
+    LOGGER.debug("Parsing data part of the file")
+    # DO NOT use comment=COMMENTS in here, if you do and the symbol is in an element for some
+    # reason then the entire parsing will crash
+    data_frame = pd.read_csv(
+        tfs_file_path,
+        engine="c",  # faster, and we do not need the features of the python engine
+        skiprows=metadata.non_data_lines,  # no need to read these lines again
+        delim_whitespace=True,  # understands ' ' is our delimiter
+        skipinitialspace=True,  # understands ' ' and '     ' are both valid delimiters
+        quotechar='"',  # elements surrounded by " are one entry -> correct parsing of strings with spaces
+        names=metadata.column_names,  # column names we have determined, avoids using first read row for columns
+        dtype=dict(
+            zip(metadata.column_names, metadata.column_types)
+        ),  # assign types at read-time to avoid re-assigning later
+    )
+
+    LOGGER.debug("Converting to TfsDataFrame")
+    tfs_data_frame = TfsDataFrame(data_frame, headers=metadata.headers)
+
+    # In pandas.read_csv an empty string ("") will be read a NaN, but we want to preserve them
+    LOGGER.debug("Ensuring preservation of empty strings")
+    for column in tfs_data_frame.select_dtypes(include=["string", "object"]):
+        tfs_data_frame[column] = tfs_data_frame[column].fillna("")
+
+    if index:
+        LOGGER.debug(f"Setting '{index}' column as index")
+        tfs_data_frame = tfs_data_frame.set_index(index)
+    else:
+        LOGGER.debug("Attempting to find index identifier in columns")
+        tfs_data_frame = _find_and_set_index(tfs_data_frame)
+
+    if validate:
+        validate_frame(tfs_data_frame, f"from file {tfs_file_path.absolute()}", non_unique_behavior)
+
+    return tfs_data_frame
+
+
+def read_headers(tfs_file_path: Union[str, pathlib.Path]) -> OrderedDict:
+    """
+    Parses the top of the **tfs_file_path** and returns the headers.
+
+    Args:
+        tfs_file_path (Union[pathlib.Path, str]): Path object to the **TFS**
+            file to read. Can be a string, in which case it will be cast to
+            a Path object.
+
+    Returns:
+        An ``OrderedDict`` with the headers read from the file.
+
+
+    Examples:
+
+        .. code-block:: python
+
+            >>> headers = read_headers("filename.tfs")
+
+        Just as with the `read_tfs` function, it is possible to load from compressed
+        files if the compression format is supported by `pandas`. The compression
+        format detection is handled automatically from the extension of the provided
+        **tfs_file_path** suffix. For instance:
+
+        .. code-block:: python
+
+            >>> headers = read_headers("filename.tfs.gz")
+    """
+    metadata: _TfsMetaData = _read_metadata(tfs_file_path)
+    return metadata.headers
+
+
+# ----- Helpers ----- #
+
+
+@dataclass
+class _TfsMetaData:
+    """A dataclass to encapsulate the metadata read from a TFS file."""
+
+    headers: OrderedDict
+    non_data_lines: int
+    column_names: np.ndarray
+    column_types: np.ndarray
+
+
+def _read_metadata(tfs_file_path: Union[str, pathlib.Path]) -> _TfsMetaData:
+    """
+    Parses the beginning of the **tfs_file_path** to extract metadata (all non dataframe lines).
+
+    .. admonition:: **Methodology**
+
+        This function parses the first lines of the file until it gets to the `types` line.
+        While parsed, the appropriate information is gathered (headers content, column names & types,
+        number of lines parsed). After reaching the `types` line, the loop is broken to avoid reading
+        the whole file. The gathered metadata is assembled in a single ``_TfsMetaData`` object and
+        returned.
+
+    Args:
+        tfs_file_path (Union[pathlib.Path, str]): Path object to the **TFS** file to read. Can be
+            a string, in which case it will be cast to a Path object.
+
+    Returns:
+        A ``_TfsMetaData`` object with the metadata read from the file.
+    """
+    LOGGER.debug("Reading headers and metadata from file")
+    tfs_file_path = pathlib.Path(tfs_file_path)
     headers = OrderedDict()
     non_data_lines: int = 0
     column_names = column_types = None
-    LOGGER.debug(f"Reading path: {tfs_file_path.absolute()}")
 
-    # First step: reading the headers, chunk by chunk (line by line) with pandas.read_csv as a context manager
-    # Very important, the value of 'sep' here should not be a value that can be found in headers (key or value)
+    # Read the headers, chunk by chunk (line by line) with pandas.read_csv as a
+    # context manager. Very important: the value of 'sep' here should not be a
+    # value that can be found in headers (key or value)
     with pd.read_csv(
         tfs_file_path,
         header=None,  # don't look for a line with column names
@@ -132,7 +246,9 @@ def read_tfs(
         engine="python",  # only engine that supports this sep argument
         dtype=str,  # we are reading the headers so we only expect and want strings, they are parsed afterwards
     ) as file_reader:
-        for line_record in file_reader:  # each read chunk / line is made into a DataFrame, colname 0 and value is the read line
+        for (
+            line_record
+        ) in file_reader:  # each read chunk / line is made into a DataFrame, colname 0 and value is the read line
             non_data_lines += 1  # important to count the line here
             try:
                 line = line_record.loc[:, 0].values[0]  # this is the value of the line as a string
@@ -153,47 +269,12 @@ def read_tfs(
             else:  # After all previous cases should only be data lines. If not, file is fucked.
                 break  # Break to not go over all lines, saves a lot of time on big files
 
-    if headers_only:
-        return TfsDataFrame(headers=headers)
-
-    if column_names is None:
-        raise TfsFormatError(f"No column names in file {tfs_file_path.absolute()}. File not read.")
-    if column_types is None:
-        raise TfsFormatError(f"No column types in file {tfs_file_path.absolute()}. File not read.")
-
-    LOGGER.debug("Parsing data part of the file")
-    # DO NOT use comment=COMMENTS in here, if you do and the symbol is in an element for some
-    # reason then the entire parsing will crash
-    data_frame = pd.read_csv(
-        tfs_file_path,
-        engine="c",  # faster, and we do not need the features of the python engine
-        skiprows=non_data_lines - 1,  # because we incremented for the first data line in loop above
-        delim_whitespace=True,  # understands ' ' is our delimiter
-        skipinitialspace=True,  # understands ' ' and '     ' are both valid delimiters
-        quotechar='"',  # elements surrounded by " are one entry -> correct parsing of strings with spaces
-        names=column_names,  # column names we have determined, avoids using first read row for columns
-        dtype=dict(zip(column_names, column_types)),  # assign types at read-time to avoid re-assigning later
+    return _TfsMetaData(
+        headers=headers,
+        non_data_lines=non_data_lines - 1,  # -1 because we incremented for the first data line
+        column_names=column_names,
+        column_types=column_types,
     )
-
-    LOGGER.debug("Converting to TfsDataFrame")
-    tfs_data_frame = TfsDataFrame(data_frame, headers=headers)
-
-    # In pandas.read_csv an empty string ("") will be read a NaN, but we want to preserve them 
-    LOGGER.debug("Ensuring preservation of empty strings")
-    for column in tfs_data_frame.select_dtypes(include=["string","object"]):
-        tfs_data_frame[column] = tfs_data_frame[column].fillna("")
-
-    if index:
-        LOGGER.debug(f"Setting '{index}' column as index")
-        tfs_data_frame = tfs_data_frame.set_index(index)
-    else:
-        LOGGER.debug("Attempting to find index identifier in columns")
-        tfs_data_frame = _find_and_set_index(tfs_data_frame)
-
-    if validate:
-        validate_frame(tfs_data_frame, f"from file {tfs_file_path.absolute()}", non_unique_behavior)
-    
-    return tfs_data_frame
 
 
 def _parse_header(str_list: List[str]) -> tuple:
