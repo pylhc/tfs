228c86ea57f389c12a5052ca53ce6d4789cae15a
Felix Soubelet <felix.soubelet@protonmail.com>
2024-08-16 11:46:04 +0200

    define a set of correct boolean headers, to be confirmed

--- tfs/constants.py
+++ tfs/constants.py
@@ -5,9 +5,12 @@ Constants
 General constants used throughout ``tfs-pandas``, relating to the standard of **TFS** files.
 """
 
+from __future__ import annotations
+
 import numpy as np
 
 # ----- TFS Format Details ----- #
+
 HEADER: str = "@"
 NAMES: str = "*"
 TYPES: str = "$"
@@ -16,7 +19,8 @@ INDEX_ID: str = "INDEX&&&"
 
 # ----- Types Mapping ----- #
 
-ID_TO_TYPE: dict[str, type] = {  # used when reading files
+# These are used when reading files
+ID_TO_TYPE: dict[str, type] = {
     "%s": str,
     "%b": bool,
     "%bpm_s": str,
@@ -25,6 +29,12 @@ ID_TO_TYPE: dict[str, type] = {  # used when reading files
     "%hd": np.int64,
     "%d": np.int64,
 }
+
+# ----- Specific Accepted Values ----- #
+
+VALID_TRUE_BOOLEANS: set[str] = {"True", "1"}  # checks resolve to capitalized case
+VALID_FALSE_BOOLEANS: set[str] = {"False", "0"}  # checks resolve to capitalized case
+VALID_BOOLEANS_HEADERS: set[str] = VALID_TRUE_BOOLEANS | VALID_FALSE_BOOLEANS
 
 # ----- Default Values ----- #
 
