bec6cc7a65007140405e5f016cb227f436e47d4e
Felix Soubelet <felix.soubelet@protonmail.com>
2024-08-16 11:36:44 +0200

    create specific exception for wrong boolean header

--- tfs/errors.py
+++ tfs/errors.py
@@ -8,3 +8,14 @@ Errors that can be raised during the handling of **TFS** files.
 
 class TfsFormatError(Exception):
     """Raised when a wrong format is detected in the **TFS** file."""
+
+
+class InvalidBooleanHeader(Exception):
+    """
+    Raised when an unaccepted boolean header
+    value is read in the **TFS** file.
+    """
+
+    def __init__(self, header_value: str) -> None:
+        errmsg = f"Invalid boolean header value parsed: '{header_value}'"
+        super().__init__(errmsg)
