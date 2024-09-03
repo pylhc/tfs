83d2264573595c8b4c8e2e703bb08bdb361cc6a0
Felix Soubelet <felix.soubelet@protonmail.com>
2024-09-03 18:58:52 +0200

    helper for asserting tfs frames equal

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,24 @@
 import pathlib
 
+import numpy as np
 import pytest
+from pandas._testing import assert_dict_equal
+from pandas.testing import assert_frame_equal
+
+from tfs import TfsDataFrame
 
 INPUTS_DIR = pathlib.Path(__file__).parent / "inputs"
+
+
+# ----- Helpers ----- #
+
+
+def assert_tfs_frame_equal(df1, df2, **kwargs):
+    assert_frame_equal(df1, df2, **kwargs)
+    assert_dict_equal(df1.headers, df2.headers, compare_keys=True)
+
+
+# ----- Fixtures ----- #
 
 
 @pytest.fixture
@@ -31,3 +47,13 @@ def _tfs_complex_file() -> pathlib.Path:
 def _tfs_madng_file() -> pathlib.Path:
     """A TFS file withboth complex values and booleans, in headers and columns."""
     return INPUTS_DIR / "madng.tfs"
+
+
+@pytest.fixture
+def _tfs_dataframe() -> TfsDataFrame:
+    return TfsDataFrame(
+        index=range(15),
+        columns="a b c d e".split(),
+        data=np.random.rand(15, 5),
+        headers={"Title": "Tfs Title", "Value": 3.3663},
+    )
