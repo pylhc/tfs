a79803dce3f7ebc8b7d0f0b4b9e78313b7f94f87
Felix Soubelet <felix.soubelet@protonmail.com>
2024-09-03 18:50:33 +0200

    add these too

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,33 @@
 import pathlib
+
 import pytest
 
-INPUT_DIR = pathlib.Path(__file__).parent / "inputs"
+INPUTS_DIR = pathlib.Path(__file__).parent / "inputs"
 
 
 @pytest.fixture
 def _tfs_filex() -> pathlib.Path:
-    return INPUT_DIR / "file_x.tfs"
+    return INPUTS_DIR / "file_x.tfs"
 
 
 @pytest.fixture
 def _tfs_filey() -> pathlib.Path:
-    return INPUT_DIR / "file_y.tfs"
+    return INPUTS_DIR / "file_y.tfs"
+
+
+@pytest.fixture
+def _tfs_booleans_file() -> pathlib.Path:
+    """A TFS file with BOOL in headers and in a column."""
+    return INPUTS_DIR / "booleans.tfs"
+
+
+@pytest.fixture
+def _tfs_complex_file() -> pathlib.Path:
+    """A TFS file with complex values in headers and in a column."""
+    return INPUTS_DIR / "complex.tfs"
+
+
+@pytest.fixture()
+def _tfs_madng_file() -> pathlib.Path:
+    """A TFS file withboth complex values and booleans, in headers and columns."""
+    return INPUTS_DIR / "madng.tfs"
