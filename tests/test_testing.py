afafcc558f57853723563ac6ec1f7327f43602bb
Felix Soubelet <felix.soubelet@protonmail.com>
2024-11-14 11:19:31 +0100

    no need to name catch the exception

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -13,27 +13,22 @@ class TestAssertTfsDataFrameEqual:
     def test_no_headers_different_data(self):
         df1 = TfsDataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
         df2 = TfsDataFrame({"a": [1, 2, 2], "b": [4, 5, 6]})
-
         with pytest.raises(AssertionError):
             assert_tfs_frame_equal(df1, df2)
 
     def test_no_headers_different_order(self):
         df1 = TfsDataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
         df2 = TfsDataFrame({"b": [4, 5, 6], "a": [1, 2, 3]})
-
         with pytest.raises(AssertionError):
             assert_tfs_frame_equal(df1, df2)
-
         assert_tfs_frame_equal(df1, df2, check_like=True)
 
     def test_with_headers_equal(self):
         df1 = TfsDataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}, headers={"a": "a", "b": "b"})
         df2 = TfsDataFrame({"b": [4, 5, 6], "a": [1, 2, 3]}, headers={"a": "a", "b": "b"})
         assert_tfs_frame_equal(df1, df1)
-
         with pytest.raises(AssertionError):
             assert_tfs_frame_equal(df1, df2)
-
         assert_tfs_frame_equal(df1, df2, check_like=True)
 
     def test_with_headers_different_data(self):
@@ -67,7 +62,7 @@ class TestAssertTfsDataFrameEqual:
     def test_with_headers_different_headers_keys(self):
         df1 = TfsDataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}, headers={"a": "a", "b": "b"})
         df2 = TfsDataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}, headers={"a": "a", "b": "b", "c": "c"})
-        with pytest.raises(AssertionError) as e:
+        with pytest.raises(AssertionError):
             assert_tfs_frame_equal(df1, df2)  # `compare_keys=True` is default
 
         # compare only common keys ---
