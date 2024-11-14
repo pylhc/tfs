cd5ef6abf2f6a00fa7a0a8af1bf093940131292b
Felix Soubelet <felix.soubelet@protonmail.com>
2024-11-14 11:11:59 +0100

    add compare_keys as parameter, with True as default

--- tfs/testing.py
+++ tfs/testing.py
@@ -19,15 +19,18 @@ if TYPE_CHECKING:
 # ----- Helpers ----- #
 
 
-def assert_tfs_frame_equal(df1: TfsDataFrame, df2: TfsDataFrame, **kwargs):
+def assert_tfs_frame_equal(df1: TfsDataFrame, df2: TfsDataFrame, compare_keys: bool = True, **kwargs):
     """
-    Assert that two `TfsDataFrame` objects are equal,
-    comparing both the contents of the headers dictionaries
-    as well as the data.
+    Compare two `TfsDataFrame` objects, this is mostly intended
+    for unit tests. Comparison is done on both the contents of
+    the headers dictionaries (with `pandas`'s `assert_dict_equal`)
+    as well as the data itself (with `pandas`'s `assert_frame_equal`).
 
     Args:
         df1 (TfsDataFrame): The first `TfsDataFrame` to compare.
         df2 (TfsDataFrame): The second `TfsDataFrame` to compare.
+        compare_keys (bool): If `True`, conpares the headers keys
+            as well. Defaults to `True`. 
         **kwargs: Additional keyword arguments are transmitted to
         `pandas.testing.assert_frame_equal` for the comparison of
         the dataframe parts themselves.
@@ -40,4 +43,4 @@ def assert_tfs_frame_equal(df1: TfsDataFrame, df2: TfsDataFrame, **kwargs):
             assert_tfs_frame_equal(reference_df, new_df)
     """
     assert_frame_equal(df1, df2, **kwargs)
-    assert_dict_equal(df1.headers, df2.headers, compare_keys=True)
+    assert_dict_equal(df1.headers, df2.headers, compare_keys=compare_keys)
