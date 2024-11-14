50c2835eb7d7e547f72f0c13a263c33a56b683c8
Felix Soubelet <felix.soubelet@protonmail.com>
2024-11-14 11:05:43 +0100

    example

--- tfs/testing.py
+++ tfs/testing.py
@@ -31,6 +31,13 @@ def assert_tfs_frame_equal(df1: TfsDataFrame, df2: TfsDataFrame, **kwargs):
         **kwargs: Additional keyword arguments are transmitted to
         `pandas.testing.assert_frame_equal` for the comparison of
         the dataframe parts themselves.
+    
+    Example:
+        .. code-block:: python
+
+            reference_df = tfs.read("path/to/file.tfs")
+            new_df = some_function(*args, **kwargs)
+            assert_tfs_frame_equal(reference_df, new_df)
     """
     assert_frame_equal(df1, df2, **kwargs)
     assert_dict_equal(df1.headers, df2.headers, compare_keys=True)
