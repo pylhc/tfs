75dbb6e18539312f541caa3e5d8d4b8c490bc6f8
Felix Soubelet <felix.soubelet@protonmail.com>
2026-01-12 15:12:23 +0100

    this pandas function expects a bool, we're lucky this evaluated to False down in their code

--- tfs/writer.py
+++ tfs/writer.py
@@ -142,7 +142,7 @@ def write_tfs(
     # would be transformed into <pd.NA> and if we write this to file we are very much cooked.)
     # Overall we do not care to infer specialized dtypes, just that it makes the best inference
     # to valid dtypes (i.e. an object column should be inferred as strings if that makes sense).
-    data_frame = data_frame.convert_dtypes(convert_integer=False, convert_floating=False, convert_string=None)
+    data_frame = data_frame.convert_dtypes(convert_integer=False, convert_floating=False, convert_string=False)
 
     if save_index:
         left_align_first_column = True
