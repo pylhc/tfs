dd969fc5682e8e065a7055c13ededfa4a52ad569
Felix Soubelet <felix.soubelet@protonmail.com>
2026-01-12 15:14:24 +0100

    fix return type hint, this can also be None if it is tasked with MAD-NG's 'nil'

--- tfs/reader.py
+++ tfs/reader.py
@@ -346,7 +346,7 @@ def _read_metadata(tfs_file_path: pathlib.Path | str) -> _TfsMetaData:
     )
 
 
-def _parse_header_line(str_list: list[str]) -> tuple[str, bool | str | int | float | np.complex128]:
+def _parse_header_line(str_list: list[str]) -> tuple[str, bool | str | int | float | np.complex128 | None]:
     """
     Parses the data in the provided header line. Expects a valid header
     line starting with the '@' identifier, and parses the content that
